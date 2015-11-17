0d91812114254434d0a69a822bb43789f2ef8f10
vjindal50 <vjindal@stevens.edu>
2015-11-16 21:22:25 -0500

    Create Rectangle.hh

---
+++ Rectangle.hh
@@ -0,0 +1,18 @@
+#ifndef RECTANGLE_HH_
+#define RECTANGLE_HH_
+
+#include "Shape.hh"
+
+class Rectangle
+{
+ private:
+	double height;
+	double width;
+	int color;
+ public:
+	void draw(int h, int w) : height(h), width(w) {}
+	void fill(int c) : color(c) {}
+	void create_border(int thickness);	
+}
+
+#endif
