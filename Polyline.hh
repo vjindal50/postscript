3ebb5edc36d82db0607e86d610fb727e37ac5372
vjindal50 <vjindal@stevens.edu>
2015-12-18 23:39:20 -0500

    updated

--- Polyline.hh
+++ Polyline.hh
@@ -3,14 +3,37 @@
 
 #include "Shape.hh"
 
-class Polyline
+class Polyline : public Shape
 {
  private:
 	int next_x;
 	int next_y;
 	
  public:
-	void stroke(int nx, int ny) : next_x(nx), next_y(ny) {}	
+	
+	void lineto(int nx, int ny)  
+	{
+		next_x=nx;
+		next_y=ny;
+		file_open();
+		file << next_x << ' ' << next_y << " lineto \n";
+		file_close();
+	}
+	void line_draw(int nx, int ny)
+	{
+		lineto(nx,ny);
+		file_open();
+		file << "stroke \n";
+		file_close();
+	}
+	
+	void drawline(int nx, int ny)
+	{
+		lineto(nx,ny);
+		file_open();
+		file << "stroke \n";
+		file_close();
+	}	
 };
 
 #endif
--- Shape.hh
+++ Shape.hh
@@ -1,14 +1,80 @@
 #ifndef SHAPE_HH_
 #define SHAPE_HH_
 
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
 class Shape
 {
+
  private:
 	int x_axis;
 	int y_axis;
+	//int colr,colg,colb;
+
  public:
-	void move_to(int x, int y) : x_axis(x), y_axis(y) {}
-	void save_to_file();	
+	ofstream file;
+	float color[2][3];
+
+	void file_open()
+	{		
+		file.open("PostScript.ps", ios::out | ios::app);
+	}
+	void file_close()
+	{
+		file.close();
+	}
+
+	void move_to(int x, int y)  
+	{
+		x_axis=x;
+		y_axis=y;
+		file_open();
+		file <<	x_axis << ' ' << y_axis << " moveto \n";
+		file_close(); 	
+	}
+
+	void clearscreen()
+	{
+		file_open();
+		file << "erasepage \n";
+		file_close(); 
+	}
+
+	void setcolor(float r, float g, float b)
+	{
+		file_open();
+		file << r << ' ' << g << ' ' << b << " setrgbcolor \n";
+		file_close();		
+	}
+
+
+	void border(float b)
+	{
+		file_open();
+		file << b << " setlinewidth \n";
+		file_close();		
+	}
+
+	int getx()
+	{
+		return x_axis;
+	}
+		
+	int gety()
+	{
+		return y_axis;
+	}
+
+	void erase()
+	{
+		file_open();
+		file << "erasepage \n";
+		file_close();
+	}
+
 };
 
 #endif
