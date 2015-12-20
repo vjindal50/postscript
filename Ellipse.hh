f0a75b54ec3e5fa401e05e49e5d1ae045f0fe604
Pratik Rathod <pratik.402@gmail.com>
2015-12-19 23:55:21 -0500

    Updated Commented Ellipse file

--- Ellipse.hh
+++ Ellipse.hh
@@ -1,32 +1,66 @@
 #ifndef ELLIPSE_HH_
+
 #define ELLIPSE_HH_
 
-#include "Circle.hh" 
+
+
+#include "Circle.hh"
+
+//***This class inherits the Circle which inherits the Arc which in turn inherits the Shape class***//
+
+
 
 class Ellipse : public Circle
+
 {
+ 
  private:
+
 	int scale_x;
+
 	int scale_y;
+
 	float radius;
+
+
  public:
+
+	//***Draw_Ellipse draws an Ellipse by setting radius, X-axis Scale, Y-axis Scale from user;***//	
 	void draw_ellipse(int sx, int sy, float r)
+
 	{
+		
 		scale_x=sx;
+
 		scale_y=sy;
+
 		radius=r;
 
+
 		file_open();
+
 		file << sx << ' ' << sy <<" scale \n";
+
 		file_close();
+
 		circle_draw(r);
+
 	}
-	
+
+
+	//***Fill_Ellipse fills a drawn Ellipse by setting RGB- default color is Black***//
 	void fill_ellipse(int sx, int sy, float r)
+
 	{
+	
 		draw_ellipse(sx,sy,r);
+
 		circle_fill(r);
+	
 	}
+
 };
+
+
 
 #endif
