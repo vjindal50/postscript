e2cebb28000df017e28aeb098487d26a6c66628d
Pratik Rathod <pratik.402@gmail.com>
2015-12-19 23:54:12 -0500

    Updated Commented Circle file

--- Circle.hh
+++ Circle.hh
@@ -3,17 +3,21 @@
 
 #include "Arc.hh"
 
+//***This class inherits Arc which in turn inherits the Shape class***//
 class Circle : public Arc 
 {
  private:
 	float radius;
 	
  public:
+	//***Circle_ draw function draws a circle by taking only Radius from the user while Start & End angle from Arc class- from user***//
 	void circle_draw(float r) 
 	{
 		radius=r; 
 		arc_draw(0.0,360.0,radius);
 	}
+
+	//***Circle_fill function fills a drawn circle by setting RGB color- default color is Black***//
 	void circle_fill(float r) 
 	{
 		radius=r;
