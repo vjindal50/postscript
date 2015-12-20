84ae2d02fbd412330765cb3b5138845a25e622a7
Pratik Rathod <pratik.402@gmail.com>
2015-12-19 23:50:21 -0500

    Updated commented Arc file

--- Arc.hh
+++ Arc.hh
@@ -6,28 +6,29 @@
 
 #include "Shape.hh"
 
+//***This class inherits the Shape class and does perform various operation on Arc***//
 
 
 class Arc : public Shape 
 
 {
- 
  private:
-
+	
 	float radius;
-
+	
 	float start_angle;
-
+	
 	float end_angle;
  
 
 public:
 	
+	//***Arc_Draw function draws an Arc by taking Starting angle, Ending angle and Radius from the user***//
 	void arc_draw(float sa, float ea, float r)
 
 	{
-		
-		
+
+
 		int x=getx();
 
 		int y=gety();
@@ -36,10 +37,10 @@ public:
 		start_angle=sa;
 
 		end_angle=ea;
-	
-		radius=r;
 
+		radius=r;
  
+		
 		file_open();
 
 		file << "newpath \n";
@@ -54,8 +55,8 @@ public:
 
 	}
 	
-
-	void draw_segment(float sa, float ea, float r)
+	//***Draw_Segment function draws a segment by taking Starting angle, Ending angle and Radius from the user***//
+  	void draw_segment(float sa, float ea, float r)
 
 	{
 		
@@ -78,20 +79,21 @@ public:
 	}
 	
 
+	//***Fill_Segment function fills a drawn segment- default color is Black***//
 	void fill_segment(float sa, float ea, float r)
-	
+
 	{
 		
 		draw_segment(sa,ea,r);
 
 		file_open();
-
+		
 		file << "fill \n";
 
 		file_close();
-
-	}
 	
+	}
+
 
 };
 
