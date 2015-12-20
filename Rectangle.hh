8b03c03b42ca1e6de953fb1b6aa3220bdcd5913a
vjindal50 <vjindal@stevens.edu>
2015-12-19 22:17:39 -0500

    header update

--- Rectangle.hh
+++ Rectangle.hh
@@ -1,18 +1,44 @@
 #ifndef RECTANGLE_HH_
 #define RECTANGLE_HH_
 
-#include "Shape.hh"
+#include "Polyline.hh"
 
-class Rectangle
+class Rectangle : public Polyline
 {
  private:
-	double height;
-	double width;
-	int color;
+	float height;
+	float width;
+
  public:
-	void draw(int h, int w) : height(h), width(w) {}
-	void fill(int c) : color(c) {}
-	void create_border(int thickness);	
+	void draw_rect(float h, float w)
+	{
+		height=h;
+		width=w;
+		
+		int x=getx();
+		int y=gety();
+		
+		lineto(x+w,y);
+		lineto(x+w,y+h);
+		lineto(x,y+h);
+
+		file_open();
+		file << "closepath \n";
+		file << "gsave \n";
+		file << "stroke \n";
+		file << "grestore ";
+		file_close(); 
+	}
+
+	void fill_rect(float h, float w) //, float r, float g, float b) 
+	{
+		draw_rect(h,w);
+		//setcolor(r,g,b);
+		file_open();		
+		//file << color[1][0] << ' ' << color[1][1] << ' ' << color[1][2] <<" setrgbcolor \n"; 
+		file << "fill \n";
+		file_close();
+	}	
 };
 
 #endif
